377dec6654d00daa8f3828d7267f292cdfd14e6f
Olivier Lajoie <olivier.lajoie@dgaginno.com>
2023-03-05 13:55:18 -0500

    setting up UI

--- app/src/main/java/ca/stayfit/MainActivity.kt
+++ app/src/main/java/ca/stayfit/MainActivity.kt
@@ -2,10 +2,22 @@ package ca.stayfit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartBtn : FrameLayout = findViewById(R.id.flStart)
+
+        flStartBtn.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Here we start the excercise.",
+                Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
