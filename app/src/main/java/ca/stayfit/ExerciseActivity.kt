c9f91cac98584d03ceffdd3dc79cffed9ea6cc83
Olivier Lajoie <olivier.lajoie@dgaginno.com>
2023-03-05 15:42:57 -0500

    Implementing the rest timer

--- app/src/main/java/ca/stayfit/ExerciseActivity.kt
+++ app/src/main/java/ca/stayfit/ExerciseActivity.kt
@@ -2,11 +2,15 @@ package ca.stayfit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import ca.stayfit.databinding.ActivityExerciseBinding
 import ca.stayfit.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private var binding: ActivityExerciseBinding? = null
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +25,46 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    private fun setupRestView() {
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "Here now we will start the excercise.",
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }.start()
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        binding = null
     }
 }
